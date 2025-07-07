32a405ec33436ba5c78204ac9f30ffa766c6cf3b
AhmadGDeeb <khamideem@gmail.com>
2025-07-07 11:52:58 +0300

    add month to server time

--- esp/views.py
+++ esp/views.py
@@ -2,26 +2,27 @@ import datetime
 
 from django.shortcuts import render
 from django.http import JsonResponse
-# from .models import Medicine
+from .models import Medicine
 
 # Create your views here.
 def server_status(request):
-    return JsonResponse({"status": "ok"})
+    return JsonResponse({"status": "OK"})
 
 def server_time(request):
     # damascus time
     dt = datetime.datetime.now()
     return JsonResponse({
         "year": dt.year,
-        "day": dt.day, 
+        "day": dt.day,
+        "month": dt.month,
         "hour": dt.hour,
         "minute": dt.minute,
         "second": dt.second,
-        "microsecond": dt.microsecond
         })
 
 def schedule(request):
+    Medicine.objects.all()
     return JsonResponse({"TODO": ""})
 
 def update_medicine_status(request, medicine_id):
-    return JsonReoinse({"TODO": ""})
+    return JsonResponse({"TODO": ""})
